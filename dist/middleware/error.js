2b020aba1a5b5363c92fd1b3dc5db5b3a281c596
mina-v <mina.bessada@vodafone.com>
2023-04-22 19:51:56 +0200

    delete dist folder

--- dist/middleware/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function errorMiddleware(err, _req, res, next) {
-    console.error(err);
-    res.status(500).json({ message: "Something went wrong" });
-    next();
-}
-exports.default = errorMiddleware;
